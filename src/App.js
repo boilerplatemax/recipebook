4f8c94ffba6f8b0ed24753084f7104baa0e110e0
MaxShapovalov <55261865+MaxShapovalov@users.noreply.github.com>
2022-09-24 12:30:08 -0500

    html and css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
-import './App.css';
+import React, {useState} from 'react';
+import './CSS/App.css';
+import RecipeList from './Components/RecipeList';
 
 function App() {
+  const [recipes, setRecipes] = useState(sampleRecipes)
+
+  const sampleRecipes =[
+    {id:1,
+    name:'Chicken',
+    servings: 3,
+    cooktime: '1:45',
+    instructions: '1. Defrost chicken\n2. Season the chicken\n3. Cook chicken\n4. Enjoy chicken',
+    ingredients:[
+      {
+        id:1,
+        name:'chicken',
+        amount:'2 lbs'
+      },
+      {
+        id:2,
+        name:'salt',
+        amount:'1 Tbs'
+      },
+    ]
+    },
+    {id:2,
+      name:'Pork',
+      servings: 4,
+      cooktime: '0:45',
+      instructions: '1. Defrost pork\n2. Season the pork\n3. Cook pork\n4. Enjoy pork',
+      ingredients:[
+        {
+          id:1,
+          name:'Pork',
+          amount:'3 lbs'
+        },
+        {
+          id:2,
+          name:'Paprika',
+          amount:'1 Tbs'
+        },
+      ]
+      }
+  ]
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <RecipeList recipes={recipes}/>
     </div>
   );
 }
